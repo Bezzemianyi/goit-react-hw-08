7a9b6686390b98aff3d825b06961d0eb89caf70d
Dima_Bezzemianyi <125562777+Bezzemianyi@users.noreply.github.com>
2025-02-06 18:36:11 +0200

    add localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import Contact from "./components/ContactList/Contact";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import userData from "./userData.json";
 
 const App = () => {
-  const [contact, setContact] = useState(userData);
+  const [contact, setContact] = useState(() => {
+    const savedData = JSON.parse(localStorage.getItem("contact"));
+    if (savedData?.length) {
+      return savedData;
+    }
+    return [];
+  });
   const [filter, setFilter] = useState("");
 
   const filteredContacts = contact.filter((item) =>
     item.name.toLowerCase().includes(filter)
   );
+  useEffect(() => {
+    localStorage.setItem("contact", JSON.stringify(contact));
+  }, [contact]);
   return (
     <div>
       <h1>Phonebook</h1>
